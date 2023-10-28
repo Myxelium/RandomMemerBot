c949a283dc68872f6139be22f1965bcde00e5fd9
Myx <info@azaaxin.com>
2023-10-28 05:31:24 +0200

    Make months more clear

--- client/web/scripts/seasonal-theme.js
+++ client/web/scripts/seasonal-theme.js
@@ -2,9 +2,9 @@ var logo = document.getElementById('logotype');
 var body = document.getElementsByTagName('body')[0];
 
 var today = new Date();
-var month = today.getMonth();
+var month = today.getMonth() + 1;
 
-if (month == 9) {
+if (month == 10) { // Halloween
     logo.src = 'assets/seasonal/logo-autumn.svg';
     body.className += ' autumn';
     document.head.innerHTML += '<link rel="stylesheet" href="assets/seasonal/autumn.css" type="text/css"/>';
